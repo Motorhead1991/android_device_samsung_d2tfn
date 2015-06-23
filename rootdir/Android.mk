b8455b0cd470be3b559f639d6788d3c70e8f34f0
Anthony Cox <motorhead1991@hotmail.com>
2015-06-22 18:48:53 -0600

    Filter init.target.rc for d2tfn only (it uses netmgrd)
    Change-Id: I098d640cb1420939c74aab7560449b9bd345b08c

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,5 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 
+ifneq ($(filter d2tfn,$(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.target.rc
 LOCAL_MODULE_TAGS  := optional
@@ -7,4 +8,4 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.target.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-
+endif
